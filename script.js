742d7d4e1132bf340dbb6abb4ad3f164ae1bf045
KieranRocha <117378904+KieranRocha@users.noreply.github.com>
2022-11-05 02:24:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hex = [0,1,2,3,4,5,6,7,8,9,"A","B",'C','D','E','F']
 const btn = document.querySelector('.botao');
 const colorName = document.querySelector('.colorName');
 const random = document.querySelector('.change');
-const hexRandom = document.querySelector('.change1');
+// const hexRandom = document.querySelector('.change1');
 btn.addEventListener("click", function() {
 
     let hexColor = "#"
@@ -16,7 +16,8 @@ btn.addEventListener("click", function() {
     colorName.textContent = hexColor;
     colorName.style.color = hexColor;
     random.style.color = hexColor;
-    hexRandom.style.color = hexColor;
+    document.querySelector('.change1').style.color = hexColor;
+    document.querySelector('#btn').style.color = hexColor;
 })
 
 function randomColor() {
